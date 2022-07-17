faf0793da9f476508b2534af616b132caeceec80
Hendriklcknr07 <hendrik@lueckner.de>
2022-07-17 17:31:19 +0200

    added password and sing in button

--- main.js
+++ main.js
@@ -1,15 +1,24 @@
-
-
 function buttoncheck(){
     let age = document.getElementById("ageNumber").value;
 
     if(age>= 12){
         console.log("Willkommen");
     } else {
-        console.log("tüs");
+        alert("error");
     }
-    
-}
+        
+        let password = document.getElementById("password").value;
+         let pass = 10;
+      
+          if(pass == password){
+                 console.log("Willkommen");
+          } else {
+                alert("Falsches password");
+          }
+      }
 
 
-document.getElementById("buttoncheck").addEventListener("click", buttoncheck);
+
+
+    document.getElementById("buttoncheck").addEventListener("click", buttoncheck);
+    document.getElementById("buttoncheckpas").addEventListener("click",buttoncheckpas);
