6350ee08d03f69c7ce91ed609099e51d99bdd5ab
Hendriklcknr07 <hendrik@lueckner.de>
2022-07-21 16:21:56 +0200

    all in one function

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 function buttoncheck(){
     let age = document.getElementById("ageNumber").value;
+    let password = document.getElementById("password").value;
+    let pass = 10;
 
-    if(age>= 12){
+    if(age>= 12 , pass==password){
         console.log("Willkommen");
     } else {
         alert("error");
     }
-        
-        let password = document.getElementById("password").value;
+} 
+       /* let password = document.getElementById("password").value;
          let pass = 10;
       
           if(pass == password){
@@ -17,6 +19,14 @@ function buttoncheck(){
           }
       }
 
+        function buttonexposed() {
+        if (!buttoncheck == true) {
+            window.open("C:\Users\Katja\Downloads\PortableGit\share\gtk-doc\html\p11-kit\index.html");
+        } else{
+            alert("alles falsch");
+            return false;
+        }
+      }*/
 
 
 
