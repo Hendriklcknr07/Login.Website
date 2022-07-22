3e9ef43c09c856a7d04f66ee26cd43512fbc5412
Hendriklcknr07 <hendrik@lueckner.de>
2022-07-22 20:11:04 +0200

    added a new file the file is open when the log in is true

--- main.js
+++ main.js
@@ -6,33 +6,11 @@ function buttoncheck(){
 
     if(age>= 12 , pass==password){
         open("index_in_new.html", "_self");
-    } else {
+        } else {
         alert("error");
     }
 
  }
 
 
-       /* let password = document.getElementById("password").value;
-         let pass = 10;
-      
-          if(pass == password){
-                 console.log("Willkommen");
-          } else {
-                alert("Falsches password");
-          }
-      }
-
-        function buttonexposed() {
-        if (!buttoncheck == true) {
-            window.open("C:\Users\Katja\Downloads\PortableGit\share\gtk-doc\html\p11-kit\index.html");
-        } else{
-            alert("alles falsch");
-            return false;
-        }
-      }*/
-
-
-
     document.getElementById("buttoncheck").addEventListener("click", buttoncheck);
-    document.getElementById("buttoncheckpas").addEventListener("click",buttoncheckpas);
